0f813b11dfcca385cecd4cc5389026dbd8181d2f
Илья Добрынин <mr.woodysimpson@gmail.com>
2025-03-18 20:11:19 +0300

    Update about.py

--- modules/about.py
+++ modules/about.py
@@ -8,7 +8,7 @@ maria = Maria()
 async def about(message: types.Message, state: FSMContext):
     await state.finish()
     users = await maria.get_users()
-    await message.answer(f"Всего пользователей: {users[0][0]}\n\nНаш сервис позволяет вам получать свое расписание в удобной для вас форме. А так же: получать расписание других групп и ваших преподавателей, смотреть расписание экзаменов, узнать список студентов другой группы, и это только начало! Дальше - больше\n\n*Бот создан @oladushik, при поддержке @botkainews и @capy_para*", parse_mode="markdown")
+    await message.answer(f"Всего пользователей: {users[0][0]}\n\nНаш сервис позволяет вам получать свое расписание в удобной для вас форме. А так же: получать расписание других групп и ваших преподавателей, смотреть расписание экзаменов, узнать список студентов другой группы, и это только начало! Дальше - больше\n\n*Бот создан @oladushik, при поддержке @botkainews и @capy_para \nАвтор идеи @dobryninilya*", parse_mode="markdown")
     
 def register(dp: Dispatcher):
     dp.register_message_handler(about, text="О боте", state="*")
